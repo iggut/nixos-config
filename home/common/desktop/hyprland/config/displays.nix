bfdd284db93645890e2b50cb6c212d86fece5092
Igor G <igor.gutchin@gmail.com>
2023-07-28 19:53:32 -0400

    display being f..ky..test

--- home/common/desktop/hyprland/config/displays.nix
+++ home/common/desktop/hyprland/config/displays.nix
@@ -1,18 +1,18 @@
 _: {
   gaminix = {
     workspace = [
-      "1, monitor: HDMI-A-1, default:true"
-      "2, monitor: HDMI-A-1"
-      "3, monitor: HDMI-A-1"
-      "4, monitor: HDMI-A-1"
-      "5, monitor: HDMI-A-1"
-      "6, monitor: DP-1"
-      "7, monitor: DP-1"
+      "1, monitor:Acer Technologies XZ322Q 1049010E43900, default:true"
+      "2, monitor:Acer Technologies XZ322Q 1049010E43900"
+      "3, monitor:Acer Technologies XZ322Q 1049010E43900"
+      "4, monitor:Acer Technologies XZ322Q 1049010E43900"
+      "5, monitor:Acer Technologies XZ322Q 1049010E43900"
+      "6, monitor:Samsung Electric Company SAMSUNG 0x01000E00, default:true"
+      "7, monitor:Samsung Electric Company SAMSUNG 0x01000E00"
     ];
 
     monitor = [
-      "DP-1,1920x1080@165,0x0,1"
-      "HDMI-A-1,2560x1440@60,1920x0,1"
+      "desc:Acer Technologies XZ322Q 1049010E43900,1920x1080@165,0x0,1"
+      "desc:Samsung Electric Company SAMSUNG 0x01000E00,2560x1440@60,1920x0,1"
     ];
   };
 
