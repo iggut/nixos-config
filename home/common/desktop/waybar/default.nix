5408b00099e2b79730233b825cf8a0e7a96f55c4
Igor G <igor.gutchin@gmail.com>
2023-07-29 20:41:31 -0400

    jnsgruk sempai fixed some stuff

--- home/common/desktop/waybar/default.nix
+++ home/common/desktop/waybar/default.nix
@@ -1,8 +1,16 @@
-{ config, pkgs, lib, hostname, desktop, theme, ... }:
-let
+{
+  config,
+  pkgs,
+  lib,
+  hostname,
+  desktop,
+  theme,
+  ...
+}: let
   # If this is a laptop, then include network/battery controls
   modules =
-    if hostname == "freyja" then [
+    if hostname == "freyja"
+    then [
       "tray"
       "network"
       "battery"
@@ -31,109 +39,120 @@ let
     on-click = "activate";
   };
 
-  inherit ((import ../rofi/lib.nix { inherit lib; })) toRasi;
-  inherit ((import ../rofi/powermenu { inherit config lib desktop pkgs theme; })) rofi-power;
-in
-{
+  inherit ((import ../rofi/lib.nix {inherit lib;})) toRasi;
+  inherit ((import ../rofi/powermenu {inherit config lib desktop pkgs theme;})) rofi-power;
+in {
   programs.waybar = {
     enable = true;
-    package = if desktop == "hyprland" then pkgs.waybar-hyprland else pkgs.waybar;
+    package =
+      if desktop == "hyprland"
+      then pkgs.waybar-hyprland
+      else pkgs.waybar;
 
     systemd = {
       enable = true; #if desktop == "sway" then true else false;
     };
 
-    settings = [{
-      exclusive = true;
-      position = "top";
-      height = 18;
-      passthrough = false;
-      gtk-layer-shell = true;
+    settings = [
+      {
+        exclusive = true;
+        position = "top";
+        layer = "top";
+        height = 18;
+        passthrough = false;
+        gtk-layer-shell = true;
 
-      modules-left = [ (if desktop == "sway" then "sway/workspaces" else "wlr/workspaces") ];
-      modules-center = [ "clock" "idle_inhibitor" ];
-      modules-right = modules;
+        modules-left = [
+          (
+            if desktop == "sway"
+            then "sway/workspaces"
+            else "wlr/workspaces"
+          )
+        ];
+        modules-center = ["clock" "idle_inhibitor"];
+        modules-right = modules;
 
-      "sway/workspaces" = workspaceConfig;
-      "wlr/workspaces" = workspaceConfig;
+        "sway/workspaces" = workspaceConfig;
+        "wlr/workspaces" = workspaceConfig;
 
-      "network" = {
-        format-wifi = "{essid} ";
-        format-ethernet = "{ifname} ";
-        format-disconnected = "";
-        tooltip-format = "{ifname} / {essid} ({signalStrength}%) / {ipaddr}";
-        max-length = 15;
-        on-click = "${pkgs.alacritty}/bin/alacritty -e ${pkgs.networkmanager}/bin/nmtui";
-      };
-
-      "idle_inhibitor" = {
-        format = "{icon}";
-        format-icons = {
-          activated = "";
-          deactivated = "";
+        "network" = {
+          format-wifi = "{essid} ";
+          format-ethernet = "{ifname} ";
+          format-disconnected = "";
+          tooltip-format = "{ifname} / {essid} ({signalStrength}%) / {ipaddr}";
+          max-length = 15;
+          on-click = "${pkgs.alacritty}/bin/alacritty -e ${pkgs.networkmanager}/bin/nmtui";
         };
-      };
 
-      "battery" = {
-        states = {
-          good = 95;
-          warning = 20;
-          critical = 10;
+        "idle_inhibitor" = {
+          format = "{icon}";
+          format-icons = {
+            activated = "";
+            deactivated = "";
+          };
         };
-        format = "{capacity}% {icon}";
-        format-charging = "{capacity}% ";
-        format-plugged = "";
-        tooltip-format = "{time} ({capacity}%)";
-        format-alt = "{time} {icon}";
-        format-full = "";
-        format-icons = [ "" "" "" "" "" ];
-      };
 
-      "tray" = {
-        icon-size = 15;
-        spacing = 10;
-      };
-
-      "clock" = { format = "{:%d %b %H:%M}"; };
-
-      "pulseaudio" = {
-        format = "{volume}% {icon}";
-        format-bluetooth = "{volume}% {icon} {format_source}";
-        format-bluetooth-muted = " {icon} {format_source}";
-        format-muted = "";
-        format-icons = {
-          headphone = "";
-          hands-free = "";
-          headset = "";
-          phone = "";
-          portable = "";
-          car = "";
-          default = [ "" "" "" ];
+        "battery" = {
+          states = {
+            good = 95;
+            warning = 20;
+            critical = 10;
+          };
+          format = "{capacity}% {icon}";
+          format-charging = "{capacity}% ";
+          format-plugged = "";
+          tooltip-format = "{time} ({capacity}%)";
+          format-alt = "{time} {icon}";
+          format-full = "";
+          format-icons = ["" "" "" "" ""];
         };
-        on-click = "${pkgs.avizo}/bin/volumectl toggle-mute";
-        on-click-right = "${pkgs.pavucontrol}/bin/pavucontrol";
-        tooltip-format = "{volume}% / {desc}";
-      };
 
-      "pulseaudio#source" = {
-        format = "{format_source}";
-        format-source = "";
-        format-source-muted = "";
-        on-click = "${pkgs.avizo}/bin/volumectl -m toggle-mute";
-        on-click-right = "${pkgs.pavucontrol}/bin/pavucontrol";
-        tooltip-format = "{source_volume}% / {desc}";
-      };
+        "tray" = {
+          icon-size = 15;
+          spacing = 10;
+        };
 
-      "custom/power" = {
-        format = "";
-        on-click = "${lib.getExe rofi-power} ${desktop}";
-      };
-    }];
+        "clock" = {format = "{:%d %b %H:%M}";};
+
+        "pulseaudio" = {
+          format = "{volume}% {icon}";
+          format-bluetooth = "{volume}% {icon} {format_source}";
+          format-bluetooth-muted = " {icon} {format_source}";
+          format-muted = "";
+          format-icons = {
+            headphone = "";
+            hands-free = "";
+            headset = "";
+            phone = "";
+            portable = "";
+            car = "";
+            default = ["" "" ""];
+          };
+          on-click = "${pkgs.avizo}/bin/volumectl toggle-mute";
+          on-click-right = "${pkgs.pavucontrol}/bin/pavucontrol";
+          tooltip-format = "{volume}% / {desc}";
+        };
+
+        "pulseaudio#source" = {
+          format = "{format_source}";
+          format-source = "";
+          format-source-muted = "";
+          on-click = "${pkgs.avizo}/bin/volumectl -m toggle-mute";
+          on-click-right = "${pkgs.pavucontrol}/bin/pavucontrol";
+          tooltip-format = "{source_volume}% / {desc}";
+        };
+
+        "custom/power" = {
+          format = "";
+          on-click = "${lib.getExe rofi-power} ${desktop}";
+        };
+      }
+    ];
 
     # This is a bit of a hack. Rasi turns out to be basically CSS, and there is
     # a handy helper to convert nix -> rasi in the home-manager module for rofi,
     # so I'm using that here to render the stylesheet for waybar
-    style = toRasi (import ./theme.nix { inherit config pkgs lib theme; }).theme;
+    style = toRasi (import ./theme.nix {inherit config pkgs lib theme;}).theme;
   };
 
   # This is a hack to ensure that hyprctl ends up in the PATH for the waybar service on hyprland
