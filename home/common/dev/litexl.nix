6694686aa6e589bae7734d63023c483611e7147b
Igor G <igor.gutchin@gmail.com>
2023-08-03 01:11:54 -0400

    working

--- home/common/dev/litexl.nix
+++ home/common/dev/litexl.nix
@@ -8,11 +8,11 @@
   ];
   home.file = {
     ".config/lite-xl/init.lua" = {
-      source = ./init.lua;
+      source = ../desktop/config/init.lua;
       recursive = true;
     };
     ".config/lite-xl/fonts/SymbolsNerdFont-Regular.ttf" = {
-      source = ./SymbolsNerdFont-Regular.ttf;
+      source = ../desktop/config/SymbolsNerdFont-Regular.ttf;
       recursive = true;
     };
   };
