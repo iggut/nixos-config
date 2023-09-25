e493bbf59d83d2e52a006b4909218754e988b0f0
Igor G <igor.gutchin@gmail.com>
2023-09-24 23:21:13 -0400

    virtiofsd test over 9000

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,44 +5,44 @@
   ght = pkgs.callPackage ./ght {};
   #waydroid-script = pkgs.callPackage ./waydroid-script {};
   #helluu = "hi";
-  qemu_7 = callPackage ./qemu_7.nix {
+  qemu_7 = pkgs.callPackage ./qemu_7.nix {
     stdenv = pkgs.ccacheStdenv;
     inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices Cocoa Hypervisor vmnet;
     inherit (pkgs.darwin.stubs) rez setfile;
     inherit (pkgs.darwin) sigtool;
   };
-  qemu_7_kvm = lib.lowPrio (pkgs.qemu_7.override {hostCpuOnly = true;});
-  qemu_7_full = lib.lowPrio (pkgs.qemu_7.override {
+  qemu_7_kvm = pkgs.qemu_7.override {hostCpuOnly = true;};
+  qemu_7_full = pkgs.qemu_7.override {
     smbdSupport = true;
     cephSupport = true;
     glusterfsSupport = true;
-  });
-  qemu_7_xen = lib.lowPrio (pkgs.qemu_7.override {
+  };
+  qemu_7_xen = pkgs.qemu_7.override {
     hostCpuOnly = true;
     xenSupport = true;
     xen = pkgs.xen-slim;
-  });
-  qemu_7_xen-light = lib.lowPrio (pkgs.qemu_7.override {
+  };
+  qemu_7_xen-light = pkgs.qemu_7.override {
     hostCpuOnly = true;
     xenSupport = true;
     xen = pkgs.xen-light;
-  });
-  qemu_7_xen_4_15 = lib.lowPrio (pkgs.qemu_7.override {
+  };
+  qemu_7_xen_4_15 = pkgs.qemu_7.override {
     hostCpuOnly = true;
     xenSupport = true;
     xen = pkgs.xen_4_15-slim;
-  });
-  qemu_7_xen_4_15-light = lib.lowPrio (pkgs.qemu_7.override {
+  };
+  qemu_7_xen_4_15-light = pkgs.qemu_7.override {
     hostCpuOnly = true;
     xenSupport = true;
     xen = pkgs.xen_4_15-light;
-  });
-  qemu_7_test = lib.lowPrio (pkgs.qemu_7.override {
+  };
+  qemu_7_test = pkgs.qemu_7.override {
     hostCpuOnly = true;
     nixosTestRunner = true;
-  });
+  };
   # TODO: when https://gitlab.com/virtio-fs/virtiofsd/-/issues/96 is fixed remove this
-  virtiofsd = callPackage ./qemu_virtiofsd.nix {
+  virtiofsd = pkgs.callPackage ./qemu_virtiofsd.nix {
     qemu = pkgs.qemu_7;
     stdenv = pkgs.ccacheStdenv;
   };
