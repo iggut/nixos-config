5de30f47612c6c778866bda62b97ec07884a79b0
Igor G <igor.gutchin@gmail.com>
2023-09-24 23:31:21 -0400

    virtiofsd windows share working

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,10 +1,7 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # Build them using 'nix build .#example' or (legacy) 'nix-build -A example'
 {pkgs ? (import ../nixpkgs.nix) {}}: {
-  #
   ght = pkgs.callPackage ./ght {};
-  #waydroid-script = pkgs.callPackage ./waydroid-script {};
-  #helluu = "hi";
   qemu_7 = pkgs.callPackage ./qemu_7.nix {
     stdenv = pkgs.ccacheStdenv;
     inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices Cocoa Hypervisor vmnet;
