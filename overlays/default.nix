3475184fabbb88e812f8c9288534b2f3522bf9e1
Igor G <igor.gutchin@gmail.com>
2023-09-24 23:13:37 -0400

    test 3

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,9 +1,4 @@
-{
-  inputs,
-  pkgs,
-  lib,
-  ...
-}: {
+{inputs, ...}: {
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs {pkgs = final;};
 
@@ -47,47 +42,5 @@
         cp -R $src/*.otf $out/share/fonts/opentype/
       '';
     };
-  };
-
-  qemu_7 = callPackage ./qemu_7.nix {
-    stdenv = pkgs.ccacheStdenv;
-    inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices Cocoa Hypervisor vmnet;
-    inherit (pkgs.darwin.stubs) rez setfile;
-    inherit (pkgs.darwin) sigtool;
-  };
-  qemu_7_kvm = lib.lowPrio (pkgs.qemu_7.override {hostCpuOnly = true;});
-  qemu_7_full = lib.lowPrio (pkgs.qemu_7.override {
-    smbdSupport = true;
-    cephSupport = true;
-    glusterfsSupport = true;
-  });
-  qemu_7_xen = lib.lowPrio (pkgs.qemu_7.override {
-    hostCpuOnly = true;
-    xenSupport = true;
-    xen = pkgs.xen-slim;
-  });
-  qemu_7_xen-light = lib.lowPrio (pkgs.qemu_7.override {
-    hostCpuOnly = true;
-    xenSupport = true;
-    xen = pkgs.xen-light;
-  });
-  qemu_7_xen_4_15 = lib.lowPrio (pkgs.qemu_7.override {
-    hostCpuOnly = true;
-    xenSupport = true;
-    xen = pkgs.xen_4_15-slim;
-  });
-  qemu_7_xen_4_15-light = lib.lowPrio (pkgs.qemu_7.override {
-    hostCpuOnly = true;
-    xenSupport = true;
-    xen = pkgs.xen_4_15-light;
-  });
-  qemu_7_test = lib.lowPrio (pkgs.qemu_7.override {
-    hostCpuOnly = true;
-    nixosTestRunner = true;
-  });
-  # TODO: when https://gitlab.com/virtio-fs/virtiofsd/-/issues/96 is fixed remove this
-  virtiofsd = callPackage ./qemu_virtiofsd.nix {
-    qemu = pkgs.qemu_7;
-    stdenv = pkgs.ccacheStdenv;
   };
 }
